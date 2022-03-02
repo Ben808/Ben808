2373770a2526a27e925916d0ae36b5987d54be83
Ben808 <48336392+Ben808@users.noreply.github.com>
2022-03-02 11:32:09 -1000

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import Phaser from 'https://codepen.io/libe2022/pen/poWbMod.js'
 import Game from 'https://codepen.io/libe2022/pen/RwLRXNN.js'
+import GameOver from 'https://codepen.io/libe2022/pen/ZEaMerJ.js'
+
 export default new Phaser.Game({
 	type: Phaser.AUTO,
 	//makes it 480x640 aspect ratio or vertical
 	width: 480,
 	height: 640,
 	//scenes used so whatever I want to show
-	scene: Game,
+	scene: [Game, GameOver],
 	//physics engine using arcade, gravity being 200
 	physics: {
     	default: 'arcade',
@@ -18,4 +20,3 @@ export default new Phaser.Game({
     	}
 	}
 });
-
